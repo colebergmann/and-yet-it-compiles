b7318d97a80d07753b0fd77f7c001ce0b8ddce89
abbywysopal <35503311+abbywysopal@users.noreply.github.com>
2019-04-17 21:46:27 -0700

    hello-world
    demo app

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+//Button.js
+
+import React from "react"
+
+/* Title function header*/
+function Button(){
+	return(
+	<div>
+		<button>
+			Today
+		</button>
+ 	</div>
+	)
+}
+
+export default Button
