f32433728c200d04c6875c051b474db92d60df68
dsimmmons <denverjamessimmons@gmail.com>
2019-04-19 12:32:33 -0700

    Update

--- ML/DLModel.py
+++ ML/DLModel.py
@@ -24,16 +24,16 @@ dataset.drop('ride-1', axis=1, inplace=True)
 dataset.drop('ride-2', axis=1, inplace=True)
 dataset.drop('ride-3', axis=1, inplace=True)
 dataset.drop('ride-4', axis=1, inplace=True)
-#dataset.drop('day_of_month', axis=1, inplace=True)
+dataset.drop('day_of_month', axis=1, inplace=True)
 #dataset.drop('day_of_week', axis=1, inplace=True)
 #dataset.drop('hour_of_day', axis=1, inplace=True)
-#dataset.drop('minute', axis=1, inplace=True)
-#dataset.drop('dlr_open', axis=1, inplace=True)
-#dataset.drop('dca_open', axis=1, inplace=True)
-#dataset.drop('dlr_close', axis=1, inplace=True)
-#dataset.drop('dca_close', axis=1, inplace=True)
-#dataset.columns = ['Wait Ride 0', 'Open Ride 0', 'Wait Ride 1', 'Open Ride 1', 'Wait Ride 2', 'Open Ride 2', 'Wait Ride 3', 'Open Ride 3', 'Wait Ride 4', 'Open Ride 4']
-dataset.columns = ['Month Day', 'Week day', 'Hour', 'Minute', 'DLR Open', 'DCA Open', 'DLR Close', 'DCA Close', 'Wait Ride 0', 'Open Ride 0', 'Wait Ride 1', 'Open Ride 1', 'Wait Ride 2', 'Open Ride 2', 'Wait Ride 3', 'Open Ride 3', 'Wait Ride 4', 'Open Ride 4']
+dataset.drop('minute', axis=1, inplace=True)
+dataset.drop('dlr_open', axis=1, inplace=True)
+dataset.drop('dca_open', axis=1, inplace=True)
+dataset.drop('dlr_close', axis=1, inplace=True)
+dataset.drop('dca_close', axis=1, inplace=True)
+dataset.columns = ['Week day', 'hour', 'Wait Ride 0', 'Open Ride 0', 'Wait Ride 1', 'Open Ride 1', 'Wait Ride 2', 'Open Ride 2', 'Wait Ride 3', 'Open Ride 3', 'Wait Ride 4', 'Open Ride 4']
+#dataset.columns = ['Month Day', 'Week day', 'Hour', 'Minute', 'DLR Open', 'DCA Open', 'DLR Close', 'DCA Close', 'Wait Ride 0', 'Open Ride 0', 'Wait Ride 1', 'Open Ride 1', 'Wait Ride 2', 'Open Ride 2', 'Wait Ride 3', 'Open Ride 3', 'Wait Ride 4', 'Open Ride 4']
 dataset.index.name = 'date'
 
 # format dataset values for network
@@ -52,10 +52,11 @@ max = scaler.data_max_
 
 # MESS WITH THIS
 # number of steps used as input
-n_steps = 5
+n_steps = 8
 
 n_rides = 5
-n_external_features = 8
+n_external_features = 2
+
 n_features = n_external_features + 2 * n_rides
 
 # MESS WITH THIS!
@@ -66,7 +67,7 @@ f_predictor = n_external_features + 2 * f_ride
 
 # MESS WITH THIS!
 # how many steps(15 minute intervals) in future to predict
-n_future_steps = 4
+n_future_steps = 1
 
 # frame as supervised learning (shift data)
 values = sts.series_to_supervised(values, n_steps, 1, n_future_steps-1)
@@ -77,7 +78,7 @@ values = values.values
 
 #CAREFULLY MESS WITH THIS! IF THERE IS NOT ENOUGH TESTING DATA THE TESTS WILL BE INACURATE
 # number of data points to use as training data(out of 30287 points). remaining data will be delegated for testing
-n_train_steps = 25000
+n_train_steps = 21000
 
 train = values[:n_train_steps, :]
 test = values[n_train_steps:, :]
@@ -85,26 +86,30 @@ test = values[n_train_steps:, :]
 n_obs = n_steps * n_features
 train_x, train_y = train[:, :n_obs], train[:, -n_features + f_predictor]
 test_x, test_y = test[:, :n_obs], test[:, -n_features + f_predictor]
+
+# format to time difference
+
+train_y = train_y - train_x[:,-n_features + f_predictor]
+test_y = test_y - test_x[:,-n_features + f_predictor]
+
+
 # reshape input to be 3D [samples, timesteps, features]
 train_x = train_x.reshape((train_x.shape[0], n_steps, n_features))
 test_x = test_x.reshape((test_x.shape[0], n_steps, n_features))
 
-print(train_x)
-print(train_y)
 
 # MESS WITH THIS
 # design network
 ####################################################################################################################
 model = ts.keras.models.Sequential()
-model.add(ts.keras.layers.LSTM(70, return_sequences=True, input_shape=(n_steps, n_features)))
-model.add(ts.keras.layers.LSTM(50, return_sequences=True))
-model.add(ts.keras.layers.LSTM(50))
+model.add(ts.keras.layers.LSTM(100, input_shape=(n_steps, n_features)))
+model.add(ts.keras.layers.Dense(50))
 model.add(ts.keras.layers.Dense(1))
 model.compile(loss='mae', optimizer='adam')
 
 # fit network
 ####################################################################################################################
-history = model.fit(train_x, train_y, epochs=100, batch_size=100, validation_data=(test_x, test_y), verbose=1, shuffle=False)
+history = model.fit(train_x, train_y, epochs=1, batch_size=60, validation_data=(test_x, test_y), verbose=1, shuffle=False)
 
 # plot training history
 ####################################################################################################################
@@ -117,14 +122,19 @@ pyplot.show()
 ####################################################################################################################
 
 pred_y = model.predict(test_x)
-
+print(type(pred_y))
+print(type(test_y))
+pred_y = pred_y + test_y
+print(pred_y)
+pred_y = pred_y.shift(n_future_steps)
+print(pred_y)
 invert_predicted_y = pred_y * (max[f_predictor] - min[f_predictor]) + min[f_predictor]
 
 invert_actual_y = test_y * (max[f_predictor] - min[f_predictor]) + min[f_predictor]
 
 # MESS WITH THIS
 # graph last n_points points of predictions (60 is last recorded day, 120 is two days ect)
-n_points = 60
+n_points = 600
 invert_predicted_y = invert_predicted_y[-n_points:]
 invert_actual_y = invert_actual_y[-n_points:]
 pyplot.figure()
