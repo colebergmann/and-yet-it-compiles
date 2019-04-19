d28fd9233dfa1979cfbcd95ed9f7a0a8a6790f51
dsimmmons <denverjamessimmons@gmail.com>
2019-04-19 16:19:47 -0700

    minor changes

--- ML/DLModel/DLModel/DLModel.py
+++ ML/DLModel/DLModel/DLModel.py
@@ -9,7 +9,6 @@ from pandas import read_csv
 from pandas import DataFrame
 from pandas import concat
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
  
 # load and format csv
