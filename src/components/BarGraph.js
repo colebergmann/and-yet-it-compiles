4ecc1e3902242c832af01eeadda2a5e92ad88042
oliviaclough <38055262+oliviaclough@users.noreply.github.com>
2019-04-19 17:41:59 -0700

    Data from CSV for park population

--- src/components/BarGraph.js
+++ src/components/BarGraph.js
@@ -10,15 +10,24 @@ class BarGraph extends Component{
 		super(props);
 		this.state = {
 			chartData:{
-				labels: ['Boston','Springfield','Cambridge'],
+				labels: ['January','February','March','April','May','June','July','August','September','October','November','December'],
 				datasets: [
 					{
-						label:'Population',
-						data:[1234567,765432,23456],
+						label:'Park Population',
+						data:[46.0645,44.5,47.19,48.367,52,065,50.633,49.742,46.45,42.767,55.065,46.467,48.276],
 						backgroundColor: [
 						'rgba(255, 99, 132, .6)',
 						'rgba(52, 162, 235, .6)',
-						'rgba(255, 206, 86, .6)'
+						'rgba(255, 206, 86, .6)',
+						'rgba(52, 162, 235, .6)',
+						'rgba(152, 162, 132, .6)',
+						'rgba(222, 200, 235, .6)',
+						'rgba(182, 162, 85, .6)',
+						'rgba(82, 92, 100, .6)',
+						'rgba(52, 190, 62, .6)',
+						'rgba(134, 77, 200, .6)',
+						'rgba(66, 190, 240, .6)',
+						'rgba(240, 132, 58, .6)'
 						]
 					}
 				]
