6bc6ec70f4b00a23995827f31260a464e7ff5f32
Cole Bergmann <colebergmann@ucsb.edu>
2019-05-02 15:13:42 -0700

    Added basic program framework to test travis with java

---
+++ data programs/LiveData/src/test/java/SuccessTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SuccessTest {
+    @Test
+    public void successfulTest() {
+        assertEquals("Zero should be zero, the universe is broken", 0, 0);
+    }
+}
