694fb01d0597a0427d59587204bcba3c6628d58d
Cole Bergmann <colebergmann@ucsb.edu>
2019-05-02 21:37:33 -0700

    Added comments

--- data programs/LiveData/src/test/java/DisneyRequestsTest.java
+++ data programs/LiveData/src/test/java/DisneyRequestsTest.java
@@ -8,6 +8,10 @@ import static org.junit.Assert.*;
 public class DisneyRequestsTest {
 
     @Test
+    /*
+        Test the expected case with a few known good URLs that we will use elsewhere in the program
+        to make sure they a) return correctly and b) the program reads them correctly
+     */
     public void normalSuccessCase() {
         DisneyRequests dr = new DisneyRequests();
 
@@ -15,15 +19,21 @@ public class DisneyRequestsTest {
         assertTrue("API key is invalid", dr.getApiKey().length() > 0);
 
         try {
-            //check known good URL
+            //check a few known good URLs
             dr.getJsonFromURL("https://api.wdpro.disney.go.com/facility-service/theme-parks/330339/wait-times");
+            dr.getJsonFromURL("https://api.wdpro.disney.go.com/facility-service/theme-parks/336894/wait-times");
         } catch (Exception e) {
             e.printStackTrace();
             fail("An exception was thrown for a known good URL");
         }
     }
 
+
     @Test(expected = IOException.class)
+    /*
+        Test the case where we input a completely invalid URL (404)
+        Should throw an IOException
+     */
     public void invalidURLCase() throws IOException {
         DisneyRequests dr = new DisneyRequests();
 
@@ -31,8 +41,14 @@ public class DisneyRequestsTest {
         dr.getJsonFromURL("https://api.wdpro.disney.go.com/facility-service/theme-parks/4444444444/wait-times");
     }
 
+
+
     @Test(expected = JSONException.class)
-    public void validButNotJsonURL() throws IOException {
+    /*
+        Test the case where we give it a valid, yet non-json URL
+        Should throw JSONException
+     */
+    public void validButNotJsonURL() throws JSONException, IOException {
         DisneyRequests dr = new DisneyRequests();
 
         //Valud URL that does not contain uson
