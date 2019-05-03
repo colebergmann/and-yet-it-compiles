da34aa4ae6a4a851cb695c84a2e0853fed3a1707
rykien42 <ryanm777@gmail.com>
2019-05-03 10:32:19 -0700

    turned predDB.py into a database class file

--- backend/predDB.py
+++ backend/predDB.py
@@ -2,50 +2,77 @@
 
 import mysql.connector
 from mysql.connector import Error, MySQLConnection
+import json
 
-def connect():
-    """connect to MySQL database"""
-    try:
-        conn = mysql.connector.connect(host='localhost', database='testDB',user='root',password='#ChandraIsGr8')
-        if conn.is_connected():
-            print("Connected to mySQL database")
-        return conn;
+class database(object):
 
-    except Error as e:
-        print(e)
+    def __init__(self, numRide):
+        self.conn = self.connect()
+        self.numRide = numRide
+        self.data = {}
 
-def disconnect(conn):
-    """disconnect from MySQL database"""
-    try:
-        conn.close()
-        if not conn.is_connected():
-            print("Disconnected from mySQL database")
 
-    except Error as e:
-        print(e)
+    def writeToJSONFile(self, path, filename, data):
+        filePathNameWExt = './' + path + '/' + filename + '.json'
+        with open(filePathNameWExt, 'a') as fp:
+            json.dump(data, fp, sort_keys=True, indent=4)
 
-def queryWithFetchone(conn):
-    try:
+    def connect(self):
+        """connect to MySQL database"""
+        try:
+            conn = mysql.connector.connect(host='localhost', database='testDB',user='root',password='#ChandraIsGr8')
+            if conn.is_connected():
+                print("Connected to mySQL database")
+            return conn;
+
+        except Error as e:
+            print(e)
+
+    def disconnect(self, conn):
+        """disconnect from MySQL database"""
+        try:
+            conn.close()
+            if not conn.is_connected():
+                print("Disconnected from mySQL database")
+
+        except Error as e:
+            print(e)
+
+    def insertPredDataArray(self, conn, numRide, data):
         cursor = conn.cursor()
-        cursor.execute("SELECT Prediction FROM ride0")
+        cursor.execute(( " ride%d" % numRide ))
+        pass
 
-        plottableData = []
+    def fetchPredDataArray(self, conn, numRide, data):
+        try:
+            cursor = conn.cursor()
+            cursor.execute(( "SELECT Prediction FROM ride%d" % numRide ))
 
-        row = cursor.fetchone()
+            plottableData = []
 
-        while row is not None:
-            plottableData.append(row)
             row = cursor.fetchone()
+            predData = row[0]
 
-    except Error as e:
-        print(e)
+            while row is not None:
+                predData = row[0]
+                plottableData.append(predData)
+                row = cursor.fetchone()
 
-    for i in range(len(plottableData)):
-        print(plottableData[i])
-    cursor.close()
+        except Error as e:
+            print(e)
+
+        data[numRide] = plottableData
+
+        self.writeToJSONFile("", "api", data)
+
+        for i in range(len(plottableData)):
+            print(plottableData[i])
+        cursor.close()
 
 
 if __name__ == '__main__':
-    conn = connect()
-    queryWithFetchone(conn)
-    disconnect(conn)
+    DB = database(1)
+    DB2 = database(0)
+    DB.fetchPredDataArray(DB.conn, DB.numRide, DB.data)
+    DB2.fetchPredDataArray(DB.conn, DB.numRide, DB.data)
+    DB.disconnect(DB.conn)
