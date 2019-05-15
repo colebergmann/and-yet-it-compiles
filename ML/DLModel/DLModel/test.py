ee935341e2efe89cdf699aa4c5fa557a720229c4
dsimmmons <denverjamessimmons@gmail.com>
2019-05-14 18:05:36 -0700

    update

--- ML/DLModel/DLModel/test.py
+++ ML/DLModel/DLModel/test.py
@@ -1,15 +1,19 @@
-from myModelsPop import myModels
+from myModels import myModels
 from matplotlib import pyplot
 import time
 import numpy as np
 
-model = myModels('C:\\Users\\denve\\OneDrive\\Documents\\MLData\\pdata.csv')
-model.setupModel('model_p14.json','model_p14.h5')
-pred = model.predict(100)
+model = myModels('data.csv')
+model.setupModel('model_53.json','model_53.h5')
+pred = model.predict(5,3)
+model.setupModel('model_56.json','model_56.h5')
+pred = np.append(pred, model.predict(5,3))
+model.setupModel('model_512.json','model_512.h5')
+pred = np.append(pred, model.predict(5,6))
+model.setupModel('model_524.json','model_524.h5')
+pred = np.append(pred, model.predict(5,12))
+model.setupModel('model_590.json','model_590.h5')
+pred = np.append(pred, model.predict(5,78))
 pyplot.figure()
-#pyplot.plot(pred)
-#pyplot.plot(pred2)
-#pyplot.plot(pred3)
-#pyplot.plot(pred4)
 pyplot.plot(pred)
 pyplot.show()
