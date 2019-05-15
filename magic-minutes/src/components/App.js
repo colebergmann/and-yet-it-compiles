f8c241def399dfc68e8185fe78b687f1ced2fd0f
Cole Bergmann <colebergmann@ucsb.edu>
2019-05-14 17:47:33 -0700

    bootstrap

--- magic-minutes/src/components/App.js
+++ magic-minutes/src/components/App.js
@@ -1,34 +1,56 @@
 //App.js
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import Plan from './Plan';
-import Today from './Today';
-import Information from './Information';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Plan from "./Plan";
+import Today from "./Today";
+import Information from "./Information";
 import Icon from "./Icon";
-
+import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/js/bootstrap.min.js';
 
 class App extends Component {
   render() {
     return (
-    <Router>
+      <Router>
         <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <a><Link to={'/information'} className="nav-link"> <Icon /> </Link></a> 
-            <a><Link to={'/'} className="nav-link"> Home  </Link></a>  
-            <a><Link to={'/today'} className="nav-link">Ride Times Today  </Link></a>
-            <a><Link to={'/plan'} className="nav-link">Plan Your Trip   </Link></a>
-          </ul>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">
+            <Icon />
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon" />
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                <a><Link to={'/'} className="nav-link"> Home  </Link></a>
+                </li>
+                <li class="nav-item">
+                  <a><Link to={'/today'} className="nav-link">Ride Times Today  </Link></a>
+                </li>
+                <li class="nav-item">
+                <a><Link to={'/plan'} className="nav-link">Plan Your Trip   </Link></a>
+                </li>
+              </ul>
+            </div>
           </nav>
 
           <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/information' component={Information} />
-              <Route path='/plan' component={Plan} />
-              <Route path='/today' component={Today} />
+            <Route exact path="/" component={Home} />
+            <Route path="/information" component={Information} />
+            <Route path="/plan" component={Plan} />
+            <Route path="/today" component={Today} />
           </Switch>
         </div>
       </Router>
